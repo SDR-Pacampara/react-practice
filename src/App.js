8a4f975f8d427ccafc89dbdcee41b409452084c5
SDR-Pacampara <sdr.pacampara@gmail.com>
2024-10-23 17:07:46 +0800

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from './Components/Button';
+import Card from './Components/Card';
+import CardCounter from './Components/CardCounter';
 
 /* function App() {
   return (
@@ -23,12 +25,56 @@ import Button from './Components/Button';
   );
 } */
 
-export default App;
 
-function App() {
+
+/* function App() {
   return (
+
     <div>
       <Button />
     </div>
   );
+} */
+
+/* function App() {
+  const cardData = {
+    card1: {
+      cardName: "John Doe",
+      description: "Software Engineer",
+    },
+    card2: {
+      cardName: "Merry Cruz",
+      description: "Marketing Expert",
+    },
+    card3: {
+      cardName: "Jason Oliver",
+      description: "Web Designer",
+    },
+  };
+
+  //const cardDescription1 = 
+  //'Lorem ipsum dolor.';
+
+  return (
+    <div className='card-container'>
+      <Card {...cardData.card1} />
+      <Card {...cardData.card2} />
+      <Card {...cardData.card3} />
+      
+      //<Card cardName={"Card 1"} description={cardDescription1}/>
+      //<Card cardName={"Card 2"} description={"Description2"}/>
+      //<Card cardName={"Card 3"} description={"Description3"}/> 
+     
+    </div>
+  );
+} */
+
+function App() {
+  return (
+    <div className="card-container">
+      <CardCounter />
+    </div>
+  );
 }
+
+export default App;
